5411e4d1c5c1f7803f17c04a365e9ed18077db23
uboston <uboston@byu.edu>
2025-04-07 12:01:26 -0600

    Removed stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,31 +6,25 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
-import ProductPage from './pages/ProductPage';
-import CartPage from './pages/CartPage';
-import { CartProvider } from './context/CartContext';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <>
-      <CartProvider>
         <Router>
           <Routes>
             <Route path="/" element={<CompetitionPage />} />
             <Route path="/competition" element={<CompetitionPage />} />
-            <Route
+            {/* <Route
               path="/product/:rootbeerName/:rootbeerId/:currentRetailPrice"
               element={<ProductPage />}
-            />
-            <Route path="/cart" element={<CartPage />} />
+            /> */}
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/signin-google" element={<Navigate to="/" />} />
           </Routes>
         </Router>
-      </CartProvider>
     </>
   );
 }
--- frontend/src/types/CartItem.ts
+++
@@ -1,6 +0,0 @@
-export interface CartItem {
-  rootbeerId: string;
-  rootbeerName: string;
-  quantity: number;
-  price: number;
-}
