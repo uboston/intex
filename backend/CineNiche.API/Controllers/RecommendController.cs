3ea5c6e0785b08ee847d551bdeeae0392717af8e
uboston <uboston@byu.edu>
2025-04-09 11:03:03 -0600

    HOG RIDER

--- backend/CineNiche.API/Controllers/RecommendController.cs
+++ backend/CineNiche.API/Controllers/RecommendController.cs
@@ -10,7 +10,7 @@ namespace CineNiche.API.Controllers;
 
 [Route("[controller]")]
 [ApiController]
-// [Authorize]
+//[Authorize]
 public class RecommendController : ControllerBase
 {
     private MoviesContext _MoviesDbContext;
