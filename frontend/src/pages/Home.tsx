2f2bf48f1f1d7f52bb6b60c17894e2d0c047d14a
Jaxon Webster <jaxonwebster4@gmail.com>
2025-04-09 22:36:39 -0600

    wednesday night shenanigans

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
         <nav className="nav-bar">
           <div className="nav-left">
             <img
-              src="/Logo.svg"
+              src="/smalllogo.png"
               alt="CineNiche Logo"
               className="primary-logo"
             />
@@ -24,17 +24,22 @@ const Home: React.FC = () => {
         <div className="hero">
           <h1>
             Welcome to{' '}
-            <img src="/wordlogo.svg" alt="CineNiche" className="center-logo" />
+            <img src="/newlogo.png" alt="CineNiche" className="center-logo" />
           </h1>
+          
+          <br></br>
           <p>Unlimited movies, TV shows, and more.</p>
           <div className="email-form">
-            <p>Ready to watch? Please Sign in above!</p>
-            <div className="get-started">
-              <input type="text" placeholder="Email address" />
-              <a href="#" className="btn-lg">
-                Get started <i className="fas fa-chevron-right"></i>
-              </a>
-            </div>
+  <div className="email-form">
+  <p>Ready to watch? Get started below!</p>
+  <div className="get-started">
+    <input type="text" placeholder="Email address" />
+    <Link to="/register" className="btn-lg">
+      Register Now <i className="fas fa-chevron-right"></i>
+    </Link>
+  </div>
+</div>
+
           </div>
         </div>
       </header>
